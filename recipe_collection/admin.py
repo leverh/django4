c3bdcd1bda8dba9b95fbcb810b4f3f14ab042d83
leverh <leverh@gmail.com>
2023-06-20 21:34:34 +0000

    Update admin.py with models. Install Pillow. Update requirements.txt with Pillow

--- recipe_collection/admin.py
+++ recipe_collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe, Profile
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'posted_by', 'date_posted')
+    list_filter = ('posted_by', 'date_posted')
+    search_fields = ('headline', 'description', 'ingredients', 'preparation')
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+    search_fields = ('user__username', 'bio')
